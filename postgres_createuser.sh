5cee3b6c81e756e9b009cbbf0f1291962fce5bf6
kakuchrome <73450989+kakuchrome@users.noreply.github.com>
2021-09-02 03:32:56 +0900

    Update postgres_createuser.sh
    add enter key input to edit hba

--- postgres_createuser.sh
+++ postgres_createuser.sh
@@ -3,6 +3,7 @@ read str
 echo  "create user and db :  $str"
 su - postgres -c "psql -U postgres -d postgres -c \"create user $str  with password 'hogehoge';\""
 su - postgres -c "psql -U postgres -d postgres -c \"create database $str ;\""
+read -p "user and db been created , name is $str , hit enter to edit hba config "
 vi /var/lib/pgsql/13/data/pg_hba.conf
 systemctl restart postgresql-13.service
 systemctl status postgresql-13.service
